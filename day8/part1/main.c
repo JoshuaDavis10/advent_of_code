2a2e23b348c3da503a9652c85dfe326d5ee81821
JoshuaDavis10 <jjdavis5760@icloud.com>
2025-12-09 01:57:11 -0600

    setup day 8 directory

---
+++ day8/part1/main.c
@@ -0,0 +1,114 @@
+typedef double f64;
+typedef float f32;
+
+typedef signed char i8;
+typedef unsigned char u8;
+typedef short i16;
+typedef unsigned short u16;
+typedef int i32;
+typedef unsigned int u32;
+typedef long long i64;
+typedef unsigned long long u64;
+
+typedef unsigned int b32;
+
+#define true 1
+#define false 0
+
+#include "../../include/linux_util.c"
+
+#define PROFILER 1
+#include "../../include/profiler.c"
+
+enum {
+	/* XXX: */
+	PARSE_STAGE_COUNT
+};
+
+typedef struct {
+	/* XXX: */
+	i32 parse_stage;
+} parse_data;
+
+static parse_data parse;
+
+int main(int argc, char **argv)
+{
+	start_profile();
+
+	if(argc != 2)
+	{
+		log_error("USAGE -> ./prog [input_file]");
+		return(-1);
+	}
+
+	u64 input_size = get_file_size(argv[1]);
+	PROFILER_START_TIMING_BANDWIDTH(file_read, input_size);
+	char *input = (char*)read_file_mmapped(argv[1]);
+	PROFILER_FINISH_TIMING_BLOCK(file_read);
+
+	if(input_size == 0)
+	{
+		log_error("input file '%s' has size: 0. terminating.", argv[1]);
+		return(-1);
+	}
+
+	if(!input)
+	{
+		log_error("could not read input from '%s'. terminating.", argv[1]);
+		return(-1);
+	}
+
+	log_trace("input:\n%s", input);
+	log_trace("input_size: %llu", input_size);
+
+	PROFILER_START_TIMING_BANDWIDTH(parse, input_size);
+	u64 input_index = 0;
+	while(input_index < input_size)
+	{
+		switch(input[input_index])
+		{
+			case '\n':
+			{
+				/* XXX: */
+			} break;
+			case ',':
+			{
+				/* XXX: */
+			} break;
+			case '0':
+			{
+				/* XXX: */
+			} break;
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+			{
+				/* XXX: */
+			} break;
+			default:
+			{
+				_assert_log(0, "unexpected char during parse: %c (0x%x)", input[input_index], input[input_index]);
+			} break;
+		}
+		input_index++;
+	}
+	PROFILER_FINISH_TIMING_BLOCK(parse);
+
+	PROFILER_START_TIMING_BLOCK(result_calculation);
+	u64 result = 0;
+	PROFILER_FINISH_TIMING_BLOCK(result_calculation);
+
+	finish_and_print_profile(log_trace);
+
+
+	log_info("result: %llu", result);
+
+	return(0);
+}
