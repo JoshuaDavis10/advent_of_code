ae61d2f9811f86787a815fd5674fcd562ad57026
JoshuaDavis10 <jjdavis5760@icloud.com>
2025-12-10 17:53:19 -0600

    I think this is my last commit. they are getting too hard for me to do without spending all day or multiple days

--- day9/part2/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-echo -e "\e[1;37mSanta's elves are building your program...\e[0;37m"
-if gcc -std=c89 -fsanitize=address main.c -o prog; then
-	echo -e "\e[1;32mThe elves have finished preparing your program!\e[0;37m"
-else
-	echo -e "\e[1;31mThe elves can't build your program because there is something deeply wrong with it!\e[0;37m"
-fi
